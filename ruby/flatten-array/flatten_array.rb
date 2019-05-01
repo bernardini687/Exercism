d725cf3ac40eff61cec291d1fbee8389933e2e4c
Oscar Bernardini <bernardini687@gmail.com>
2019-05-01 23:04:49 +0200

    Custom flatten for arrays of integers

--- ruby/flatten-array/flatten_array.rb
+++ ruby/flatten-array/flatten_array.rb
@@ -1,6 +1,7 @@
 module FlattenArray
   def self.flatten(array)
-    return array unless array.include?(nil)
+    return array if array.none? { |e| e.is_a?(Array) }
 
+    array.to_s.tr('[]nil,', '').split.map(&:to_i)
   end
 end
