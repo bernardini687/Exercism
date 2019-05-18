8c73e08da3fb17106b4da6532c1625d4ccf92807
Oscar Bernardini <bernardini687@gmail.com>
2019-05-18 16:55:41 +0200

    Unlock tests

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,14 @@
+const COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'blue', 'violet', 'grey', 'white'];
+
+const colorCode = new Map();
+colorCode.set('black', 0);
+colorCode.set('brown', 0);
+colorCode.set('red', 0);
+colorCode.set('orange', 0);
+colorCode.set('yellow', 0);
+colorCode.set('blue', 0);
+colorCode.set('violet', 0);
+colorCode.set('grey', 0);
+colorCode.set('white', 0);
+
+export { COLORS, colorCode };
