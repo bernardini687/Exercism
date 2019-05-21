4c7774d69b2c4311afa2a847bdd22c43f4073220
Oscar Bernardini <bernardini687@gmail.com>
2019-05-21 14:34:32 +0200

    Pass first test

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,3 +1,7 @@
 module ResistorColorDuo
   COLORS = %w[black brown red orange yellow green blue violet grey white].freeze
+
+  def self.value(colors)
+    colors.map { |color| COLORS.index(color) }.join.to_i
+  end
 end
