a3143d6eb907a31869a0ef378953c5b8b168e101
Oscar Bernardini <bernardini687@gmail.com>
2019-05-23 09:19:09 +0200

    Change COLORS into hash

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,5 +1,16 @@
 module ResistorColorDuo
-  COLORS = %w[black brown red orange yellow green blue violet grey white].freeze
+  COLORS = {
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9
+  }.freeze
 
   def self.value(colors)
     colors.map { |color| COLORS.index(color) }.join.to_i
