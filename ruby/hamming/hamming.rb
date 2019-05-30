ed60782db50d3dec1de5b781843b0003cd1ff650
Oscar Bernardini <bernardini687@gmail.com>
2019-05-30 22:32:01 +0200

    Raise ArgumentError

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,8 +1,8 @@
-require 'pry'
-
 module Hamming
   def self.compute(string1, string2)
-    raise 'Should be of the same length' if string1.length != string2.length
+    if string1.length != string2.length
+      raise ArgumentError, 'Should be of the same length'
+    end
 
     string1.chars.zip(string2.chars).reduce(0) do |acc, curr|
       curr[0] != curr[1] ? acc + 1 : acc
