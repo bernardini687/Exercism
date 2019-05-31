d927194cd795335325ef2ffccdca2526a459cd97
Oscar Bernardini <bernardini687@gmail.com>
2019-05-31 08:50:01 +0200

    Remove 'pry'

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,14 +1,4 @@
-require 'pry'
 module Hamming
-  # def self.compute(str0, str1)
-  #   if str0.length != str1.length
-  #     raise ArgumentError, 'Should be of the same length'
-  #   end
-
-  #   str0.chars.zip(str1.chars).reduce(0) do |count, str|
-  #     str[0] != str[1] ? count + 1 : count
-  #   end
-  # end
   def self.compute(strand1, strand2)
     unless strand1.length == strand2.length
       raise ArgumentError, 'Should be of the same length'
