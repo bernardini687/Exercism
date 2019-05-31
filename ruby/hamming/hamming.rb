4efd1e17a5e31229a19e74bb07ecf707ced94b16
Oscar Bernardini <bernardini687@gmail.com>
2019-05-31 16:43:52 +0200

    Custom error

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,8 +1,12 @@
+class StrandLengthError < ArgumentError
+  def message
+    'Strand lengths must be identical.'
+  end
+end
+
 module Hamming
   def self.compute(strand1, strand2)
-    unless strand1.length == strand2.length
-      raise ArgumentError, 'Should be of the same length'
-    end
+    raise StrandLengthError unless strand1.length == strand2.length
 
     nucleotides = strand1.chars.zip(strand2.chars)
     nucleotides.count { |n1, n2| n1 != n2 }
