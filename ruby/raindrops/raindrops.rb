a3319070668baae9282c985ff43df79680117642
Oscar Bernardini <bernardini687@gmail.com>
2019-06-01 10:38:59 +0200

    Refactor and alias #convert

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,8 +1,25 @@
 module Raindrops
-  def self.convert(number, output = '')
-    output += 'Pling' if (number % 3).zero?
-    output += 'Plang' if (number % 5).zero?
-    output += 'Plong' if (number % 7).zero?
-    output == '' ? number.to_s : output
+  SOUNDS = {
+    3 => 'Pling',
+    5 => 'Plang',
+    7 => 'Plong'
+  }.freeze
+
+  class
+    def self.convert(number)
+      sound = ''
+      sound << SOUNDS[3] if (number % 3).zero?
+      sound << SOUNDS[5] if (number % 5).zero?
+      sound << SOUNDS[7] if (number % 7).zero?
+      sound.empty? ? number.to_s : sound
+    end
+
+    alias sound_of convert
   end
+end
+
+if $PROGRAM_NAME == __FILE__
+  p Raindrops.sound_of(28)
+  p Raindrops.sound_of(35)
+  p Raindrops.sound_of(42)
 end
