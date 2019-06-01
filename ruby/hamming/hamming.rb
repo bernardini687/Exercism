fe278f0ac9f8352eaeba8618bf152f0994f6073d
Oscar Bernardini <bernardini687@gmail.com>
2019-06-01 09:52:04 +0200

    Refactor and alias method

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,14 +1,18 @@
 class StrandLengthError < ArgumentError
-  def message
-    'Strand lengths must be identical.'
+  def initialize(message = 'Strand lengths must be identical.')
+    super
   end
 end
 
 module Hamming
-  def self.compute(strand1, strand2)
-    raise StrandLengthError unless strand1.length == strand2.length
+  class << self
+    def compute(strand1, strand2)
+      raise StrandLengthError unless strand1.length == strand2.length
 
-    nucleotides = strand1.chars.zip(strand2.chars)
-    nucleotides.count { |n1, n2| n1 != n2 }
+      nucleotides = strand1.chars.zip(strand2.chars)
+      nucleotides.count { |n1, n2| n1 != n2 }
+    end
+
+    alias difference compute
   end
 end
