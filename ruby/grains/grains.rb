cfdbf1fcd4ebe72c0f4e92217e5599cf55526c9d
Oscar Bernardini <bernardini687@gmail.com>
2019-06-07 16:54:50 +0200

    Draft quick solution

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,2 +1,15 @@
-module Grain
+require 'pry'
+module Grains
+  GRAINS = (1..64).to_a.each_with_object([]) do |cur, memo|
+    val = memo.empty? ? cur : memo[-1] * 2
+    memo << val
+  end
+
+  binding.pry
+
+  def self.square(number)
+    GRAINS[number]
+  end
 end
+
+p Grains.square(64) if $PROGRAM_NAME == __FILE__
