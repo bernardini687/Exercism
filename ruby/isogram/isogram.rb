c561ba56dd0b6feb0c5b7f3a70a2975b6a4a2471
Oscar Bernardini <bernardini687@gmail.com>
2019-06-09 13:14:48 +0200

    Raise custom error for invalid arguments

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -1,8 +1,18 @@
+class ArgumentIsNoString < ArgumentError
+  def initialize(message = 'The argument must be a String.')
+    super
+  end
+end
+
 module Isogram
   def self.isogram?(input)
+    raise ArgumentIsNoString unless input.is_a? String
+
     string = input.gsub(/[-\s]/, '')
     string ||= input
     string.downcase!
     string.length == string.chars.uniq.length
   end
 end
+
+p Isogram.isogram?(4) if $PROGRAM_NAME == __FILE__
