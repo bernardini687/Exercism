a24ede7436cadf4787cfe1cf952967a3874babec
Oscar Bernardini <bernardini687@gmail.com>
2019-06-09 21:18:11 +0200

    Change parameter's name

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -1,11 +1,11 @@
 module Isogram
-  def self.isogram?(input)
-    unless input.is_a? String
+  def self.isogram?(string)
+    unless string.is_a? String
       raise ArgumentError, 'The argument must be a String.'
     end
 
-    string = input.downcase.scan(/\w/)
-    string.length == string.uniq.length
+    letters = string.downcase.scan(/\w/)
+    letters.length == letters.uniq.length
   end
 end
 
