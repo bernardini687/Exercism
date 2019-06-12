ca76df301a931c8d6248976f202c9e0ac833e9f8
Oscar Bernardini <bernardini687@gmail.com>
2019-06-12 11:34:28 +0200

    Complete Scrabble Score's first draft

--- ruby/scrabble-score/scrabble_score.rb
+++ ruby/scrabble-score/scrabble_score.rb
@@ -1,12 +1,12 @@
 class Scrabble
   RULES = [
-    [A, E, I, O, U, L, N, R, S, T, 1],
-    [D, G, 2],
-    [B, C, M, P, 3],
-    [F, H, V, W, Y, 4],
-    [K, 5],
-    [J, X, 8],
-    [Q, Z, 10]
+    ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T', 1],
+    ['D', 'G', 2],
+    ['B', 'C', 'M', 'P', 3],
+    ['F', 'H', 'V', 'W', 'Y', 4],
+    ['K', 5],
+    ['J', 'X', 8],
+    ['Q', 'Z', 10]
   ].freeze
 
   def initialize(word)
@@ -14,5 +14,11 @@ class Scrabble
   end
 
   def score
+    @word.upcase.chars.map { |char| value_of(char) }.sum
+  end
+
+  def value_of(letter)
+    value = RULES.select { |letters| letters.include?(letter) }
+    value.empty? ? 0 : value.flatten[-1]
   end
 end
