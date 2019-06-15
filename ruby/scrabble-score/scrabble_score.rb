f4cd9807efc14ce1d04bbc98d91335d91853c6f0
Oscar Bernardini <bernardini687@gmail.com>
2019-06-15 18:24:25 +0200

    Change RULES into Hash

--- ruby/scrabble-score/scrabble_score.rb
+++ ruby/scrabble-score/scrabble_score.rb
@@ -1,13 +1,13 @@
 class Scrabble
-  RULES = [
-    ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T', 1],
-    ['D', 'G', 2],
-    ['B', 'C', 'M', 'P', 3],
-    ['F', 'H', 'V', 'W', 'Y', 4],
-    ['K', 5],
-    ['J', 'X', 8],
-    ['Q', 'Z', 10]
-  ].freeze
+  RULES = {
+    %w[A E I O U L N R S T] => 1,
+    %w[D G] => 2,
+    %w[B C M P] => 3,
+    %w[F H V W Y] => 4,
+    %w[K] => 5,
+    %w[J X] => 8,
+    %w[Q Z] => 10
+  }.freeze
 
   def initialize(word)
     @word = word || ''
@@ -28,7 +28,7 @@ class Scrabble
   private
 
   def score_of(letter)
-    score = RULES.find { |letters| letters.include?(letter) }
+    score = RULES.find { |letters, _value| letters.include?(letter) }
     score.nil? ? 0 : score[-1]
   end
 end
