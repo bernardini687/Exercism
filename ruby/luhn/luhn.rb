6e968590d62bb9701a476e63a0a133bbb4f23d69
Oscar Bernardini <bernardini687@gmail.com>
2019-07-10 20:18:34 +0200

    No more refinements

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,27 +1,21 @@
-module LuhnOps
-  refine String do
-    def has_too_many_digits?
-      size <= 1
-    end
-
-    def has_invalid_digits?
-      match?(/\D/)
-    end
-  end
-end
-
 class Luhn
-  using LuhnOps
-
   def self.valid?(number)
     number = number.gsub(/\s+/, '')
-    return false if number.has_too_many_digits?
-    return false if number.has_invalid_digits?
+    return false if too_many_chars?(number)
+    return false if no_digits?(number)
 
     (sum(number) % 10).zero?
   end
 
   private
+
+  def self.too_many_chars?(number)
+    number.size <= 1
+  end
+
+  def self.no_digits?(number)
+    number.match?(/\D/)
+  end
 
   def self.sum(number)
     reverse_pairs(number).flat_map do |pair|
