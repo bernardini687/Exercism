616e266e74bcc8b2ba7f447351e0faa6a640c66a
Oscar Bernardini <bernardini687@gmail.com>
2019-07-10 15:41:32 +0200

    Change method name

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,6 +1,6 @@
 module Provable
   refine String do
-    def too_many_digits?
+    def has_too_many_digits?
       size <= 1
     end
 
@@ -15,7 +15,7 @@ class Luhn
 
   def self.valid?(number)
     number = number.gsub(/\s+/, '')
-    return false if number.too_many_digits?
+    return false if number.has_too_many_digits?
     return false if number.has_invalid_digits?
 
     (sum(number) % 10).zero?
