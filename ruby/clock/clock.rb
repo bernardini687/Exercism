2235ef596e273e9d9b2d455682efe1693561bc81
Oscar Bernardini <bernardini687@gmail.com>
2019-07-14 15:31:43 +0200

    Reorder

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -1,39 +1,11 @@
 class Clock
   DAY, HOUR = 24, 60
-  attr_accessor :raw_hour
   attr_reader :raw_minute
+  attr_accessor :raw_hour
 
   def initialize(time)
-    @raw_hour = time[:hour] || 0
     @raw_minute = time[:minute] || 0
-  end
-
-  def hour
-    @hour ||= begin
-      if raw_hour > DAY
-        roll_over_hour(raw_hour, :-)
-      elsif raw_hour == DAY
-        0
-      elsif raw_hour < 0
-        roll_over_hour(raw_hour, :+)
-      else
-        raw_hour
-      end
-    end
-  end
-
-  def minute
-    @minute ||= begin
-      if raw_minute > HOUR
-        roll_over_minute(raw_minute, :-)
-      elsif raw_minute == HOUR
-        increment_hour
-      elsif raw_minute < 0
-        roll_over_minute(raw_minute, :+)
-      else
-        raw_minute
-      end
-    end
+    @raw_hour = time[:hour] || 0
   end
 
   def to_s
@@ -60,6 +32,34 @@ class Clock
 
   private
 
+  def minute
+    @minute ||= begin
+      if raw_minute > HOUR
+        roll_over_minute(raw_minute, :-)
+      elsif raw_minute == HOUR
+        increment_hour
+      elsif raw_minute < 0
+        roll_over_minute(raw_minute, :+)
+      else
+        raw_minute
+      end
+    end
+  end
+
+  def hour
+    @hour ||= begin
+      if raw_hour > DAY
+        roll_over_hour(raw_hour, :-)
+      elsif raw_hour == DAY
+        0
+      elsif raw_hour < 0
+        roll_over_hour(raw_hour, :+)
+      else
+        raw_hour
+      end
+    end
+  end
+
   def increment_hour
     self.raw_hour += 1
     0
@@ -67,16 +67,6 @@ class Clock
 
   def zero_pad(number)
     '%02d' % number
-  end
-
-  def roll_over_hour(time, op)
-    if time < DAY && time >= 0
-      return time
-    elsif time == DAY
-      return 0
-    end
-
-    roll_over_hour(time.send(op, DAY), op)
   end
 
   def roll_over_minute(time, op)
@@ -92,5 +82,15 @@ class Clock
       self.raw_hour -= 1
     end
     roll_over_minute(time.send(op, HOUR), op)
+  end
+
+  def roll_over_hour(time, op)
+    if time < DAY && time >= 0
+      return time
+    elsif time == DAY
+      return 0
+    end
+
+    roll_over_hour(time.send(op, DAY), op)
   end
 end
