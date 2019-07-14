50edf8f218e0a8cf05d1972f6acfca6a67b98714
Oscar Bernardini <bernardini687@gmail.com>
2019-07-14 11:58:58 +0200

    Implement recursive hour count

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -1,9 +1,21 @@
 class Clock
-  attr_reader :hour, :minute
+  attr_reader :raw_hour, :minute
 
   def initialize(time)
-    @hour = time[:hour] || 0
+    @raw_hour = time[:hour] || 0
     @minute = time[:minute] || 0
+  end
+
+  def hour
+    @hour ||= begin
+      if raw_hour > 24
+        roll_over(raw_hour, 24)
+      elsif raw_hour == 24
+        0
+      else
+        raw_hour
+      end
+    end
   end
 
   def to_s
@@ -12,5 +24,14 @@ class Clock
 
   def zero_pad(number)
     "%02d" % number
+  end
+
+  private
+
+  def roll_over(time, amount)
+    return time if time <= amount
+
+    less = time - amount
+    roll_over(less, amount)
   end
 end
