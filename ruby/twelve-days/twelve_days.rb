db65be34790d248687c201a748cf86e077a2ec70
Oscar Bernardini <bernardini687@gmail.com>
2019-08-02 11:05:51 +0200

    Add a class to deal with the list of items

--- ruby/twelve-days/twelve_days.rb
+++ ruby/twelve-days/twelve_days.rb
@@ -1,20 +1,29 @@
-class TwelveDays
-  GIFTS = {
-    first: 'a Partridge in a Pear Tree',
-    second: 'two Turtle Doves',
-    third: 'three French Hens'
-  }
+module CumulativeSong
+  class TwelveDays
+    GIFTS = {
+      first: 'a Partridge in a Pear Tree',
+      second: 'two Turtle Doves',
+      third: 'three French Hens'
+    }
 
-  GIVER = 'my true love'
+    GIVER = 'my true love'
 
-  attr_reader :gifts, :giver
+    attr_reader :gifts, :giver
 
-  def initialize(gifts: nil, giver: nil)
-    @gifts = gifts || GIFTS
-    @giver = giver || GIVER
+    def initialize(gifts: nil, giver: nil)
+      @gifts = gifts || GIFTS
+      @giver = giver || GIVER
+    end
+
+    def self.song
+
+    end
   end
 
-  def self.song
-
+  class List
+    def initialize(list: [])
+      raise ArgumentError, "#{list} must be an Array" unless list.is_a? Array
+      @list = list
+    end
   end
 end
