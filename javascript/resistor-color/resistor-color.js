33b33b689b2d41bf589713d50cc9e6338846273b
Oscar Bernardini <bernardini687@gmail.com>
2019-09-02 10:52:23 +0200

    remove javascript folder

--- javascript/resistor-color/resistor-color.js
+++
@@ -1,4 +0,0 @@
-const COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
-const colorCode = color => COLORS.indexOf(color);
-
-export { COLORS, colorCode };
--- javascript/reverse-string/reverse-string.js
+++
@@ -1,6 +0,0 @@
-export const reverseString = word => {
-  let reverseWord = '';
-  for (let index = word.length; index > 0; index--)
-    reverseWord += word[index - 1];
-  return reverseWord;
-};
