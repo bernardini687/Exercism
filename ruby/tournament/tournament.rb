9706375d48c808d0a01d2483ddd1240bd1a4579b
Oscar Bernardini <bernardini687@gmail.com>
2019-09-03 13:13:18 +0200

    pseoudocode tournament parsing

--- ruby/tournament/tournament.rb
+++ ruby/tournament/tournament.rb
@@ -1,20 +1,62 @@
-class Tournament
-  attr_reader :match_results
+require 'pry'
 
+class Tournament
   def self.tally(input)
     new(input).tally
   end
 
   def initialize(match_results)
     @match_results = match_results
+    @match_data = {}
   end
 
+  attr_reader :match_results
+  attr_accessor :match_data
+
   def tally
-    [header, *rows].join "\n"
+    return header + "\n" if no_data?
+
+    [header, parsed_rows].join "\n"
+  end
+
+  def no_data?
+    match_results.nil? || match_results.empty?
+  end
+
+  def parsed_rows
+    rows.map { |row| parse row }
   end
 
   def rows
-    input.split "\n"
+    match_results.split "\n"
+  end
+
+  def parse(match)
+    first_team, second_team, result = match.split ';'
+    # if result win
+    # match_data[first_team] = wins
+    # match_data[second_team] = loses
+    # if draw
+    # match_data[first_team] = ties
+    # match_data[second_team] = ties
+    # else
+    # match_data[first_team] = loses
+    # match_data[second_team] = wins
+  end
+
+  # use these as masks to increment the results
+  # lastly calculate the points
+
+  def wins
+    { mp: 1, won: 1, drawn: 0, lost: 0 }
+  end
+
+  def ties
+    { mp: 1, won: 0, drawn: 1, lost: 0 }
+  end
+
+  def loses
+    { mp: 1, won: 0, drawn: 0, lost: 1 }
   end
 
   def header
