701f473f2ce4e1936388aaf75318170d3d3cea16
Oscar Bernardini <bernardini687@gmail.com>
2019-10-19 12:54:16 +0200

    try a different solution

--- elixir/secret-handshake/lib/secret_handshake.ex
+++ elixir/secret-handshake/lib/secret_handshake.ex
@@ -1,4 +1,6 @@
 defmodule SecretHandshake do
+  @msg [:rev, "jump", "close your eyes", "double blink", "wink"]
+
   @doc """
   Determine the actions of a secret handshake based on the binary
   representation of the given `code`.
@@ -15,12 +17,15 @@ defmodule SecretHandshake do
   """
   @spec commands(code :: integer) :: list(String.t())
   def commands(code) do
-    bits = to_bits(code)
+    bits =
+      code
+      |> to_bits
+      |> Enum.with_index
 
-    if length(bits) == 5 do
-      decode(bits, [])
-    else
-      []
+    for {bit, index} <- bits,
+        bit == "1" do
+      {action, _} = List.pop_at(@msg, index)
+      action
     end
   end
 
@@ -28,29 +33,5 @@ defmodule SecretHandshake do
     Integer.to_string(int, 2)
     |> String.pad_leading(5, "0")
     |> String.split("", trim: true)
-  end
-
-  defp decode([r, j, c, d, "1"], message) do
-    decode([r, j, c, d, nil], ["wink" | message])
-  end
-
-  defp decode([r, j, c, "1", _], message) do
-    decode([r, j, c, nil, nil], ["double blink" | message])
-  end
-
-  defp decode([r, j, "1", _, _], message) do
-    decode([r, j, nil, nil, nil], ["close your eyes" | message])
-  end
-
-  defp decode([r, "1", _, _, _], message) do
-    decode([r, nil, nil, nil, nil], ["jump" | message])
-  end
-
-  defp decode([r, _, _, _, _], message) do
-    if r == "1" do
-      message
-    else
-      Enum.reverse(message)
-    end
   end
 end
