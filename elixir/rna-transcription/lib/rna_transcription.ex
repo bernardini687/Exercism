1b0375c5fc212f4f2138c4454ac5c60ecb1fe511
Oscar Bernardini <bernardini687@gmail.com>
2019-10-24 17:57:00 +0200

    download new elixir exercise

---
+++ elixir/rna-transcription/lib/rna_transcription.ex
@@ -0,0 +1,21 @@
+defmodule RnaTranscription do
+  @translator %{?G => ?C, ?C => ?G, ?T => ?A, ?A => ?U}
+
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+  iex> RnaTranscription.to_rna('ACTG')
+  'UGAC'
+  """
+  @spec to_rna([char]) :: [char]
+  def to_rna(dna) do
+    Enum.map(
+      dna,
+      fn dna_n ->
+        %{^dna_n => rna_n} = @translator
+        rna_n
+      end)
+  end
+end
