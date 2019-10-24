4d7a41af746273e29e354c6bfdf529698c983dd8
Oscar Bernardini <bernardini687@gmail.com>
2019-10-24 21:23:33 +0200

    work in progress for rotational cipher

--- elixir/rotational-cipher/lib/rotational_cipher.ex
+++ elixir/rotational-cipher/lib/rotational_cipher.ex
@@ -11,8 +11,16 @@ defmodule RotationalCipher do
   def rotate(text, shift) do
     # secret = for char <- to_charlist(text), do: char + shift
     text
-    |> to_charlist
-    |> Enum.map &(&1 + shift)
+    |> to_charlist()
+    |> Enum.map(&foo(&1, shift))
     |> to_string()
   end
+
+  def foo(char, 0), do: char
+  def foo(char, 26), do: char
+  def foo(char, shift) when char + shift in ?A..?Z, do: char + shift
+  def foo(char, shift) when char + shift in ?a..?z, do: char + shift
+  def foo(char, shift) when char in ?A..?Z, do: (?A - 1) + (char + shift - ?Z)
+  def foo(char, shift) when char in ?a..?z, do: (?a - 1) + (char + shift - ?z)
+  def foo(char, shift), do: char
 end
