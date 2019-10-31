24f56682f87b4adee1ef60d58ae42cfb705732ca
Oscar Bernardini <bernardini687@gmail.com>
2019-10-31 12:12:52 +0100

    first solution for rotational cipher

--- elixir/rotational-cipher/lib/rotational_cipher.ex
+++ elixir/rotational-cipher/lib/rotational_cipher.ex
@@ -9,18 +9,40 @@ defmodule RotationalCipher do
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
-    # secret = for char <- to_charlist(text), do: char + shift
-    text
-    |> to_charlist()
-    |> Enum.map(&foo(&1, shift))
-    |> to_string()
+    do_rotate(text, shift)
   end
 
-  def foo(char, 0), do: char
-  def foo(char, 26), do: char
-  def foo(char, shift) when char + shift in ?A..?Z, do: char + shift
-  def foo(char, shift) when char + shift in ?a..?z, do: char + shift
-  def foo(char, shift) when char in ?A..?Z, do: (?A - 1) + (char + shift - ?Z)
-  def foo(char, shift) when char in ?a..?z, do: (?a - 1) + (char + shift - ?z)
-  def foo(char, shift), do: char
+  defp do_rotate(text, 0), do: text
+
+  defp do_rotate(text, shift) when shift > 0 do
+    String.replace(
+      text,
+      ~r/[a-zA-Z]/,
+      fn <<char>> ->
+        if char in ?a..?z do
+          case calc_shift(char, shift, :lower) do
+            0 ->
+              char + shift
+
+            n ->
+              96 + n
+          end
+        else
+          case calc_shift(char, shift, :upper) do
+            0 ->
+              char + shift
+
+            n ->
+              64 + n
+          end
+        end
+      end
+    )
+  end
+
+  defp calc_shift(char, shift, :lower),
+    do: Integer.mod(char + shift - 96, ?z - 96)
+
+  defp calc_shift(char, shift, :upper),
+    do: Integer.mod(char + shift - 64, ?Z - 64)
 end
